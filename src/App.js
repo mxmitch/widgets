df7556fa7e11bdb0b31547a3161323f3c1c887d5
Mitch Lum <mklum88@gmail.com>
2021-12-06 23:01:46 -0800

    add debounce with second useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end javascript framework."
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers."
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components."
-  }
-];
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework."
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers."
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components."
+//   }
+// ];
 
 export default () => {
   return (
-    <h1>
-      <br />
-      <Accordion items={items} />
-    </h1>
+    <div className="ui container">
+      <div className="ui row">
+        <Search />
+      </div>
+    </div>
   );
 };
