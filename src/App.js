8fdb613b01357e32b0acbd62d3e4c215aff65f9a
Mitch Lum <mklum88@gmail.com>
2021-12-11 16:36:00 -0800

    added dropdown component and label data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //   {
@@ -17,11 +18,26 @@ import Search from "./components/Search";
 //   }
 // ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red"
+  },
+  {
+    label: "The Color Green",
+    value: "greed"
+  },
+  {
+    label: "The Color Blue",
+    value: "blue"
+  }
+];
+
 export default () => {
   return (
     <div className="ui container">
       <div className="ui row">
-        <Search />
+        <Dropdown options={options} />
       </div>
     </div>
   );
