d76fc353560e24aa116bcbb91739fd1515f7ec55
Mitch Lum <mklum88@gmail.com>
2021-12-14 22:37:39 -0800

    add useRef to close dropdown on body click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
-
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework."
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite JS library among engineers."
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components."
-//   }
-// ];
 
 const options = [
   {
@@ -34,10 +19,22 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div className="ui container">
       <div className="ui row">
-        <Dropdown options={options} />
+        <button onClick={() => setShowDropdown(!showDropdown)}>
+          Toggle Dropdown
+        </button>
+        {showDropdown ? (
+          <Dropdown
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options}
+          />
+        ) : null}
       </div>
     </div>
   );
