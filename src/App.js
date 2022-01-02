640a4b701d572ab57328f6ea1148aa92da0adf44
Mitch Lum <mklum88@gmail.com>
2022-01-02 00:29:58 -0800

    added Route component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
+import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const options = [
   {
@@ -19,11 +20,68 @@ const options = [
   }
 ];
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework."
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers."
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components."
+  }
+];
+
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+
 export default () => {
+  const [selected, setSelected] = useState("");
+
   return (
     <div className="ui container">
       <div className="ui row">
-        <Translate />
+        <Route path="/">
+          <Accordion items={items} />
+        </Route>
+        <Route path="/list">
+          <Search />
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown
+            label="Select a color"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+          />
+        </Route>
+        <Route path="/translate">
+          <Translate />
+        </Route>
       </div>
     </div>
   );
